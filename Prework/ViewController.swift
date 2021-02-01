14e1c5841dd1dcfc7ad03c4aae9669c36c048833
Nicholas Palermo <nicholas.palermo2@cix.csi.cuny.edu>
2021-01-31 23:17:49 -0500

    Comments added

--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -9,13 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    //Creates any neccessary connections with view controller on Main.storyboard
     @IBOutlet weak var billAmountTextField: UITextField!
     @IBOutlet weak var tipAmountLabel: UILabel!
     @IBOutlet weak var tipControl: UISegmentedControl!
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var billPerPersonLabel: UILabel!
     @IBOutlet weak var rateLabel: UILabel!
-    @IBOutlet weak var rateSlider: UISlider!
+    @IBOutlet weak var tipSlider: UISlider!
     @IBOutlet weak var partySizeStepper: UIStepper!
     @IBOutlet weak var partySizeLabel: UILabel!
     
@@ -26,34 +28,38 @@ class ViewController: UIViewController {
         
     }
     
+    //action changes partySizeLabel for party size based on partySizeStepper
     @IBAction func partySizeSelection(_ sender: Any) {
         
         partySizeLabel.text = "\(Int(partySizeStepper.value))"
     }
     
-    @IBAction func tipRateSelection(_ sender: Any) {
-        let tipPercentages = [0.15, 0.18, 0.20]
-        let rate = tipPercentages[tipControl.selectedSegmentIndex]
-        rateSlider.value = Float(rate)
-        rateLabel.text = "\(Int(rateSlider.value*100))%"
+    //action uses tipControl to manipulate rateLabel and tip rate
+    @IBAction func tipRateSelectionWithSegementedControl(_ sender: Any) {
+        let tipPercentages = [0.15, 0.18, 0.20]                             //creates a constant array for the set of predetermined tip percentages decimal conversions
+        let rate = tipPercentages[tipControl.selectedSegmentIndex]          //creates a constant for rate selected by segmented control
+        tipSlider.value = Float(rate)                                       //sets the tipSlider to the rate chosen by segmented control
+        rateLabel.text = "\(Int(tipSlider.value*100))%"                     //updates rateLabel to chosen segmented control value
     }
     
+    //action uses tipSlider
     @IBAction func tipRateSelectionWithSlider(_ sender: Any) {
-        rateLabel.text = "\(Int(rateSlider.value*100))%"
+        rateLabel.text = "\(Int(tipSlider.value*100))%"                     //updates rateLabel to chosen slider value
     }
     
+    
+    //action calculates the tip and the bill total and splits it by chosen party size
     @IBAction func calculateTip(_ sender: UIButton) {
-    // Retreive bill amount from text field input
+    
+        //generates neccesary constants for inputted bill amount, tip rate, and party size
         let bill = Double(billAmountTextField.text!) ?? 0
-        
-        let rate = rateSlider.value
-        
-        
+        let rate = tipSlider.value
         let partySize = partySizeStepper.value
         
-        let tip = bill * Double(rate)
-        let total = bill + tip
-        let billPerPerson = total/partySize
+        //performs neccessary computations for tip calculation
+        let tip = bill * Double(rate)                                       //calculates tip
+        let total = bill + tip                                              //calculates total
+        let billPerPerson = total/partySize                                 //divides total by number of party members
         
         
         //Update tip amount label
